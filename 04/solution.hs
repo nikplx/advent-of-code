90326bcc657d94fc6b37b678cd33487a2da6d718
Niklas Paulicks <niklas.paulicks@cancom.de>
2020-12-10 10:19:22 +0100

    some more hacking on 04

--- 04/solution.hs
+++ 04/solution.hs
@@ -1,11 +1,10 @@
 -- |
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Data.Text.Internal.Search
-import Data.Text (pack)
-import Data.List
+import Prelude hiding (lookup)
+import Data.Map
+import Text.ParserCombinators.ReadP
 
 data Passport = Passport { birthYear    :: Int,
                            issueYear    :: Int,
@@ -15,17 +14,39 @@ data Passport = Passport { birthYear    :: Int,
                            eyeColour    :: String,
                            pId          :: String,
                            cId          :: Maybe String
-                         }
+                         } deriving (Show)
 
-readPassport :: String -> Passport
-readPassport s = undefined
 
-readField :: String -> String -> Maybe String
-readField q s
-  | null idx = Nothing
-  | otherwise = Just $ drop (head idx) s
+splitColon :: ReadP (String, String)
+splitColon = do
+  key <- munch (/=':')
+  _ <- string ":"
+  value <- munch (/=' ')
+  _ <- string " "
+  return (key, value)
+
+
+pairParser :: ReadP [(String, String)]
+pairParser = many splitColon
+
+readPass :: String -> Maybe Passport
+readPass s = do
+  bY <- lookupNum "byr" m
+  iY <- lookupNum "iyr" m
+  eY <- lookupNum "eyr" m
+  h <- lookupNum "hgt" m
+  hC <- lookup "hcl" m
+  eC <- lookup "ecl" m
+  p <- lookup "pid" m
+  c <- lookup "cid" m
+
+  return (Passport bY, iY, eY, h, hC, eC, p, c)
   where
-    idx = indices (pack q) (pack s)
+    m = (fromList . fst . last . readP_to_S pairParser) s
+
+lookupNum :: String -> Map String String -> Maybe Int
+lookupNum s m = read <$> lookup s m
+
 
 main :: IO ()
 main = undefined
